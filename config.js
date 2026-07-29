64bd6b9b95d22ad7f027dd8c33d7fc7a8188ecaa
brazaorafael <brazaorafael@gmail.com>
2026-07-29 20:15:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 /**
- * Este é o único arquivo que normalmente precisa ser editado à mão.
+ * Este Ã© o Ãºnico arquivo que normalmente precisa ser editado Ã  mÃ£o.
  * Cole abaixo a URL exibida pelo Cloudflare depois de publicar o Worker.
  */
 window.COZINHA_CONFIG = {
-  WORKER_URL: "",
+  WORKER_URL: "https://cozinha-a-dois-worker.brazaorafael.workers.dev",
   NOME_CASAL: "Ana & Rafael",
   CACHE_VERSION: "cozinha-v2",
 };
